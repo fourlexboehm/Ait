1ae6e6c6ff174ee7d97b4369acf00ac32cc1966f
Alex Boehm <alexb@ozrunways.com>
2021-12-11 13:34:24 +1030

    Revert "Updated files"
    This reverts commit bd6b2b06837f2654b7c7728069e3484218ca74cb.
    
    to broke things

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: aboehm <aboehm@42adel.org.au>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 15:09:12 by aboehm            #+#    #+#             */
-/*   Updated: 2021/12/11 13:23:47 by aboehm           ###   ########.fr       */
+/*   Updated: 2021/12/08 15:16:36 by aboehm           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,9 @@
 
 int main(int argc, char **argv)
 {
-    if (argc == 5)
+    if (argc == 3)
     {
-		printf("%s", *argv[1]);
-		printf("%s", *argv[1]);
-		printf("%s", *argv[1]);
-		printf("%s", *argv[1]);
+		ft_printf("%s", *argv[1]);
 
     }
 }
--- main.c
+++ main.c
@@ -1,11 +1,10 @@
+#include "libft/libft.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
 	char set[] = "\t \n";
-	char s1[] = "\t lorem ipsuka";
-	char s1[] = "\t lorem ipsum";
 	char s1[] = "\t lorem ipsum";
 
 	printf("%s", s1);
